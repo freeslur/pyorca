89f743e85e31d25a43d10a73bed2ecf57a8bb3e9
freeslur <freeslur@gmail.com>
2020-08-25 10:00:01 +0900

    a

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-from orcalib import system_info, patient
-
 import pprint
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from orcalib import patient, system_info
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
--- orcalib/patient.py
+++ orcalib/patient.py
@@ -1,15 +1,13 @@
+import pprint
+
 import requests
 import xmltodict
 
 import orcalib.orca_default as orca
 
-import pprint
-import sys
-
 result = xmltodict.parse(
     requests.get(
-        url=orca.default_url + orca.patient_basic_info + "id=00000",
-        auth=orca.auth,
+        url=orca.default_url + orca.patient_basic_info + "id=00000", auth=orca.auth,
     ).content
 )["xmlio2"]["patientinfores"]
 
@@ -17,22 +15,23 @@ result = xmltodict.parse(
 def delete(request_data):
     req_data = request_data["data"]
     pprint.pprint(req_data)
-    post_data = orca.post_param_default("patientmodreq",
-                                        ("<Patient_ID type='string'>"+req_data["id"]+"</Patient_ID>"
-                                         "<WholeName type='string'>" +
-                                         req_data["name"]+"</WholeName>"
-                                         "<WholeName_inKana type='string'>" +
-                                         req_data["name_kana"] +
-                                         "</WholeName_inKana>"
-                                         "<BirthDate type='string'>" +
-                                         req_data["birth_date"]+"</BirthDate>"
-                                         "<Sex type='string'>"+req_data["sex"]+"</Sex>")
-                                        )
+    post_data = orca.post_param_default(
+        "patientmodreq",
+        (
+            "<Patient_ID type='string'>" + req_data["id"] + "</Patient_ID>"
+            "<WholeName type='string'>" + req_data["name"] + "</WholeName>"
+            "<WholeName_inKana type='string'>"
+            + req_data["name_kana"]
+            + "</WholeName_inKana>"
+            "<BirthDate type='string'>" + req_data["birth_date"] + "</BirthDate>"
+            "<Sex type='string'>" + req_data["sex"] + "</Sex>"
+        ),
+    )
     res = requests.delete(
         url=orca.default_url + orca.delete_patient,
         data=post_data.encode("utf-8"),
         headers=orca.post_headers,
-        auth=orca.auth
+        auth=orca.auth,
     ).content
     pprint.pprint("OK??")
     return res
@@ -47,22 +46,23 @@ def delete(request_data):
 def regist(request_data):
     req_data = request_data["data"]
     pprint.pprint(req_data)
-    post_data = orca.post_param_default("patientmodreq",
-                                        ("<Patient_ID type='string'>"+req_data["id"]+"</Patient_ID>"
-                                         "<WholeName type='string'>" +
-                                         req_data["name"]+"</WholeName>"
-                                         "<WholeName_inKana type='string'>" +
-                                         req_data["name_kana"] +
-                                         "</WholeName_inKana>"
-                                         "<BirthDate type='string'>" +
-                                         req_data["birth_date"]+"</BirthDate>"
-                                         "<Sex type='string'>"+req_data["sex"]+"</Sex>")
-                                        )
+    post_data = orca.post_param_default(
+        "patientmodreq",
+        (
+            "<Patient_ID type='string'>" + req_data["id"] + "</Patient_ID>"
+            "<WholeName type='string'>" + req_data["name"] + "</WholeName>"
+            "<WholeName_inKana type='string'>"
+            + req_data["name_kana"]
+            + "</WholeName_inKana>"
+            "<BirthDate type='string'>" + req_data["birth_date"] + "</BirthDate>"
+            "<Sex type='string'>" + req_data["sex"] + "</Sex>"
+        ),
+    )
     res = requests.post(
         url=orca.default_url + orca.regist_patient,
         data=post_data.encode("utf-8"),
         headers=orca.post_headers,
-        auth=orca.auth
+        auth=orca.auth,
     ).content
     pprint.pprint("OK??")
     return res
